f2169f899fcef01521253d3b1d1ca69a1b09147f
Sergey-Malevich <161680885+Sergey-Malevich@users.noreply.github.com>
2024-05-10 13:40:23 +0800

    Add files via upload

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -29,6 +29,9 @@ class Author(models.Model):
 class Category(models.Model):
     category = models.CharField(max_length=30, unique=True)
 
+    def __str__(self):
+        return self.category
+
 
 class Post(models.Model):
     author = models.ForeignKey('Author', on_delete=models.CASCADE)
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import *
 
 # Create your views here.
+
+class NewsListView(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = 'time_in'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'contact.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'newslist'
+
+
